5e33e32cc3eb30378a9e6b01e33a1b6a49728b5c
Towa <30366556+towan912@users.noreply.github.com>
2023-01-30 14:17:59 +0900

    [add]Add datetime expressions
    Signed-off-by: Towa <30366556+towan912@users.noreply.github.com>

---
+++ django_datetime_expressions/expressions.py
@@ -0,0 +1,61 @@
+from django.db.models.expressions import Func
+from django.db.models.fields import DateTimeField, IntegerField
+
+
+class RelativeFunc(Func):
+    output_field = DateTimeField()
+    template = (
+        "%(expressions)s + CAST('%(value)i %(convert_type)s' as INTERVAL)"
+    )
+
+    def __init__(self, value, convert_type, output_field=None):
+        super().__init__(output_field=output_field)
+        self.value = value
+        self.convert_type = convert_type
+
+
+class RelativeDay(RelativeFunc):
+    """Coerce an expression to a new field type."""
+
+    def __init__(self, value):
+        super().__init__(value, convert_type='days')
+
+
+class RelativeWeek(RelativeFunc):
+    """Coerce an expression to a new field type."""
+
+    def __init__(self, value):
+        super().__init__(value, convert_type='weeks')
+
+
+class RelativeMonth(RelativeFunc):
+    """Coerce an expression to a new field type."""
+
+    def __init__(self, value):
+        super().__init__(value, convert_type='months')
+
+
+class RelativeHour(RelativeFunc):
+    """Coerce an expression to a new field type."""
+
+    def __init__(self, value):
+        super().__init__(value, convert_type='hours')
+
+
+class RelativeMinute(RelativeFunc):
+    """Coerce an expression to a new field type."""
+
+    def __init__(self, value):
+        super().__init__(value, convert_type='minutes')
+
+
+class RelativeSecond(RelativeFunc):
+    """Coerce an expression to a new field type."""
+
+    def __init__(self, value):
+        super().__init__(value, convert_type='seconds')
+
+
+class Epoch(Func):
+    template = 'EXTRACT(epoch FROM %(expressions)s)::INTEGER'
+    output_field = IntegerField()
